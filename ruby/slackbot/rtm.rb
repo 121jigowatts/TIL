891369485387948996ee064c3fd9127fae22e0c5
soil <sh.yoshida@live.jp>
2019-04-30 18:49:23 +0900

    about Real Time Messaging API

---
+++ ruby/slackbot/rtm.rb
@@ -0,0 +1,33 @@
+require 'http'
+require 'json'
+require 'faye/websocket'
+require 'eventmachine'
+
+response = HTTP.post("https://slack.com/api/rtm.start", params: {
+    token: ENV['SLACK_API_TOKEN']
+  })
+
+# puts JSON.pretty_generate(JSON.parse(response.body))
+
+rc = JSON.parse(response.body)
+url = rc['url']
+
+EM.run do
+  ws = Faye::WebSocket::Client.new(url)
+
+  ws.on :open do
+    p [:open]
+  end
+
+  ws.on :message do |event|
+    p [:message, JSON.parse(event.data)]
+  end
+
+  ws.on :close do
+    p [:close, event.code]
+      ws = nil
+      EM.stop
+  end
+end
+
+
