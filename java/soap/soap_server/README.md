39ef13ef67969ffb1efa32d6e3b1e3ee8d578db9
soil <sh.yoshida@live.jp>
2020-04-15 22:54:05 +0900

    add user inquiry

---
+++ java/soap/soap_server/README.md
@@ -0,0 +1,18 @@
+# SOAP Server
+
+## Start up
+
+```sh
+./mvnw clean install
+./mvnw spring-boot:run
+```
+
+## Request
+
+```sh
+# Country情報照会
+curl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws
+
+# User情報照会
+curl --header "content-type: text/xml" -d @request_user.xml http://localhost:8080/ws
+```
---
+++ java/soap/soap_server/src/main/java/com/jigowatts/soap_server/service/users/UserEndpoint.java
@@ -0,0 +1,32 @@
+package com.jigowatts.soap_server.service.users;
+
+import com.jigowatts.soap_server.GetUserRequest;
+import com.jigowatts.soap_server.GetUserResponse;
+import com.jigowatts.soap_server.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class UserEndpoint {
+    private static final String NAMESPACE_URI = "http://jigowatts.com/soap_server";
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserEndpoint(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getUserRequest")
+    @ResponsePayload
+    public GetUserResponse getUser(@RequestPayload GetUserRequest request) {
+        GetUserResponse response = new GetUserResponse();
+        response.setUser(userRepository.findUser(request.getId()));
+
+        return response;
+    }
+}
