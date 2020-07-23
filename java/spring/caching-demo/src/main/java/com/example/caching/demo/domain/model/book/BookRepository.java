9632256c0d717a8399370291ba48a264655683aa
soil <sh.yoshida@live.jp>
2020-07-23 21:38:57 +0900

    add cache clear

--- java/spring/caching-demo/src/main/java/com/example/caching/demo/domain/model/book/BookRepository.java
+++ java/spring/caching-demo/src/main/java/com/example/caching/demo/domain/model/book/BookRepository.java
@@ -2,4 +2,6 @@ package com.example.caching.demo.domain.model.book;
 
 public interface BookRepository {
     Book getByIsbn(String isbn);
+
+	void evict(String isbn);
 }
--- java/spring/caching-demo/src/main/java/com/example/caching/demo/infrastructure/repository/SimpleBookRepository.java
+++ java/spring/caching-demo/src/main/java/com/example/caching/demo/infrastructure/repository/SimpleBookRepository.java
@@ -3,9 +3,13 @@ package com.example.caching.demo.infrastructure.repository;
 import com.example.caching.demo.domain.model.book.Book;
 import com.example.caching.demo.domain.model.book.BookRepository;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class SimpleBookRepository implements BookRepository {
     @Override
@@ -13,6 +17,12 @@ public class SimpleBookRepository implements BookRepository {
     public Book getByIsbn(String isbn) {
         simulateSlowService();
         return new Book(isbn, "Some book");
+    }
+
+    @CacheEvict(cacheNames = "books")
+    public void evict(String isbn) {
+        // Do nothing.
+        log.info("Cache clear.");
     }
 
     // Don't do this at home
