b4d8c7180abaf16f6dfb65e380310c779d17cba6
soil <sh.yoshida@live.jp>
2020-07-26 22:26:12 +0900

    add kotlin special classes

---
+++ kotlin/example/special_classes/objectKeyword.kt
@@ -0,0 +1,53 @@
+import java.util.Random
+
+class LuckDispatcher {
+    fun getNumber() {
+        var objRandom = Random()
+        println(objRandom.nextInt(90))
+    }
+}
+
+// object Expression
+fun rentPrice(standardDays: Int, festivityDays: Int, specialDays: Int) {
+    val dayRates = object {
+        var standard: Int = 30 * standardDays
+        var festivity: Int = 50 * festivityDays
+        var special: Int = 100 * specialDays
+    }
+
+    val total = dayRates.standard + dayRates.festivity + dayRates.special
+
+    println("Total price: $$total")
+}
+
+// object Declaration
+object DoAuth {
+    fun takeParams(username: String, password: String) {
+        println("input Auth parameters = $username:$password")
+    }
+}
+
+// Compainon Objects
+class BigBen {
+    companion object Bonger {
+        fun getBongs(nTimes: Int) {
+            for (i in 1..nTimes) {
+                print("BONG ")
+            }
+        }
+    }
+}
+
+fun main() {
+    val d1 = LuckDispatcher()
+    val d2 = LuckDispatcher()
+
+    d1.getNumber()
+    d2.getNumber()
+
+    rentPrice(10, 2, 1)
+
+    DoAuth.takeParams("foo", "qwerty")
+
+    BigBen.getBongs(12)
+}
