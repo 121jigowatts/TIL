5d90d300b100ba8c92c5ea385b378809ed515134
soil <sh.yoshida@live.jp>
2020-07-27 09:26:08 +0900

    add kotlin functional

---
+++ kotlin/example/functional/higherOrderFunctions.kt
@@ -0,0 +1,20 @@
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
+    return operation(x, y)
+}
+
+fun mySum(x: Int, y: Int) = x + y
+
+fun operation(): (Int) -> Int {
+    return ::square
+}
+
+fun square(x: Int) = x * x
+
+fun main() {
+    val sumResult = calculate(4, 5, ::mySum)
+    val mulResult = calculate(4, 5) { a, b -> a * b }
+    println("sumResult $sumResult, mulResult $mulResult")
+
+    val func = operation()
+    println(func(2))
+}
