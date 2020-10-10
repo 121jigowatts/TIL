76a4700813ab5a4403f1aef48349904fe0f30413
soil <sh.yoshida@live.jp>
2020-10-10 13:53:34 +0900

    add python package

---
+++ python/basis/package/README.md
@@ -0,0 +1,7 @@
+# ソースコードの配布
+
+`setup.py`でパッケージ化してソースコードを配布する。
+
+```sh
+python setup.py sdist
+```
---
+++ python/basis/package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='package',
+    version='1',
+    packages=['my_package'],
+    url='https://github.com/121jigowatts',
+    license='free',
+    author='soil',
+    author_email='',
+    description='python demo'
+)
