86c24de08edf388983002c2755194b4264811d6d
soil <sh.yoshida@live.jp>
2020-10-18 17:24:54 +0900

    sample console_app v2

--- python/sample/console_application/main.py
+++ python/sample/console_application/main.py
@@ -1,10 +1,15 @@
 # Udemy task
-from roboter import robot
+from roboter.controller import robot_controller
+from roboter.model import ranking
+from roboter.model import robot
 
-def action():
-    r = robot.Robot('roboco')
-    r.execute()
+
+def main():
+    roboco = robot.RestaurantRobot('roboco')
+    rank_model = ranking.Ranking()
+    restaurant_robot = robot_controller.RobotController(roboco, rank_model)
+    restaurant_robot.survey()
 
 
 if __name__ == '__main__':
-    action()
+    main()
