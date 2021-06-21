c7659517d353d8534d2f0dc68d9a286f10c44c73
soil <sh.yoshida@live.jp>
2021-06-21 17:10:35 +0900

    implement CRUD

--- node/webapi/README.md
+++ node/webapi/README.md
@@ -1,5 +1,7 @@
 # Create a new Express framework web application
 
+[Node.jsとExpressでWeb APIを作成する](https://docs.microsoft.com/en-us/learn/modules/build-web-api-nodejs-express/1-introduction)チュートリアル。
+
 ## 環境構築
 
 ```sh
--- node/webapi/app.js
+++ node/webapi/app.js
@@ -2,23 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/products", (req, res) => {
-  const products = [
-    {
-      id: 1,
-      name: "hammer",
-    },
-    {
-      id: 2,
-      name: "screwdriver",
-    },
-    {
-      id: 3,
-      name: "wrench",
-    },
-  ];
+app.use(express.json());
 
-  res.json(products);
-});
+let products = [];
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app
+  .route("/products")
+  .get((req, res) => {
+    res.json(products);
+  })
+  .post((req, res) => {
+    // スプレッド構文 (...): 配列やオブジェクトを展開することができるÏ
+    const newProduct = { ...req.body, id: products.length + 1 };
+    products = [...products, newProduct];
+    res.json(newProduct);
+  })
+  .put((req, res) => {
+    let updatedProduct;
+    products = products.map((p) => {
+      if (p.id === req.body.id) {
+        // オブジェクトのマージ
+        updatedProduct = { ...p, ...req.body };
+        return updatedProduct;
+      }
+      return p;
+    });
+    res.json(updatedProduct);
+  })
+  .delete((req, res) => {
+    // 単項演算子(plus): 数値でないオペランドの数値への変換を試みる
+    const deletedProduct = products.find((p) => p.id === +req.body.id);
+    products = products.filter((p) => p.id !== +req.body.id);
+    res.json(deletedProduct);
+  });
+
 app.listen(port, () => console.log(`Example app listening on prot ${port}!`));
