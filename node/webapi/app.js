ae406bcaad82ef22e113b5ead0e21172b2da0bcb
soil <sh.yoshida@live.jp>
2021-06-21 21:52:42 +0900

    authorization

--- node/webapi/app.js
+++ node/webapi/app.js
@@ -8,6 +8,15 @@ let products = [];
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.get("/users", isAuthorized, (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "User Userson",
+    },
+  ]);
+});
+
 app
   .route("/products")
   .get((req, res) => {
@@ -52,3 +61,13 @@ app.get("/products/:id", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function isAuthorized(req, res, next) {
+  const auth = req.headers.authorization;
+  if (auth === "secretpassword") {
+    next();
+  } else {
+    res.status(401);
+    res.send("Not permitted");
+  }
+}
